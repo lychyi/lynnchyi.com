4ed139b56dfbb85b434b91b27576413118af6f7b
Lynn Chyi <lynn.chyi@workday.com>
2019-01-02 18:05:06 -0800

    adjust SEO tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Lynn Chyi" keywords={[`gatsby`, `react`, `design technologist`, `hard edge abstraction`]} />
     <RandomCover />
   </Layout>
 )
