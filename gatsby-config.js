344c90393f8c5eaaa6070a236e66a5bfe51db430
Lynn Chyi <14299381+lychyi@users.noreply.github.com>
2019-07-20 10:07:59 -0700

    address security vulnerability
    remove gatsby-plugin-offline plugin, wasn't using it anyway

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,8 +34,5 @@ module.exports = {
         icon: `src/images/lynnchyi-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 }
